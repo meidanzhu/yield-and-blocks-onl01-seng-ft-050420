e993781d18ea5f530b3e2c6e7c587e5246a16058
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 22:16:10 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,9 @@
 def hello_t(array)
-  array = ["Tim", "Tom", "Jim"]
-  hello_t(["Tim", "Tom", "Jim"]) do |name|
+
+  array.each do |name|
     if name.start_with?("T")
-      puts "Hi, #{name}"
+        puts "Hi, #{name}"
     end
   end
-end
-
 
 # call your method here!
