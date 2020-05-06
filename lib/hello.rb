528e9884e782cca279d0f1415903b703be8d170c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 22:18:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,21 @@
 def hello_t(array)
+  i = 0
 
-  array.each do |name|
-    if name.start_with?("T")
-        puts "Hi, #{name}"
-    end
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
 end
-# call your method here!
+
+def hello_t(array)
+  if block_given?
+    for i in 0...array.length
+      if array[i].start_with?("T")
+        yield array[i]
+      end
+    end
+  else
+    puts "Please enter a block!"
+  end
+  array
+end
